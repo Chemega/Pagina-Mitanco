d090d604c5802dc1e869fb69ea93516ccc3cb85e
Chemega <garll03100@gmail.com>
2024-10-31 23:53:08 -0300

    primer actualizacion
    er

---
+++ js/consignas.js
@@ -0,0 +1,12 @@
+async function pedirConsigna(){
+
+    
+    if (pythonCheckboxPedir.checked == 1){
+        alert("checked")
+    }
+}
+
+async function crearConsigna(){
+    inputTitulo = document.querySelector("#tituloCrearConsigna")
+    inputDetalles = document.querySelector("#detallesCrearConsigna")
+}
